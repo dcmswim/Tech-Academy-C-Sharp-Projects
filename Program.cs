d29cb6b79b64b56f5dd972b6708544d0f9731c42
David <dcmswim@gmail.com>
2021-08-01 15:19:58 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
