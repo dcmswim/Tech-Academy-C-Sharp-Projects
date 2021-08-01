2b2eb2bf473e2d3b12118554bc229da83824eeee
David <dcmswim@gmail.com>
2021-08-01 14:35:00 -0700

    very basic practice with C# writing to console and escape characters

---
+++ EscapeChar.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+class Program
+{
+    static void Main()
+    {   //First practice with C# 
+        //Using line break and escape characters to display simple message
+        Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, we're \"accounting\" on you!");
+        Console.ReadLine();
+    }
+}
+
