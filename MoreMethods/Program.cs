063c62eb071c84998ef8ec3551959bfc85319afb
David <82915335+dcmswim@users.noreply.github.com>
2021-08-07 16:23:18 -0700

    Calling methods
    Practice calling methods in main program from different class with various data types.

---
+++ MoreMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MoreMethods
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int opInt = 15;
+            Math.mathTime(opInt);
+
+            decimal opDec = 22.57m;
+            Math.mathTime(opDec);
+
+            string opString = "32";
+            Math.mathTime(opString);
+
+            Console.ReadLine();
+        }
+    }
+}
