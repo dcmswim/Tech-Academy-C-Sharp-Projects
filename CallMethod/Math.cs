3f6c738b0fe1887e14b8212987df5081e03e82be
David <82915335+dcmswim@users.noreply.github.com>
2021-08-07 15:27:22 -0700

    Calling methods
    Basic practice calling methods from classes and passing variables into methods as parameters

---
+++ CallMethod/Math.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallMethod
+{   //defines class
+    public class Math
+    {   //method within class Math
+        public static void add(int opNum)
+        {
+            int num1 = 15;
+            int res1 = num1 + opNum;
+            Console.WriteLine("Addition math result = " + res1);
+        }
+        //method within class Math
+        public static void mult(int opNum)
+        {
+            int num2 = 12;
+            int res2 = num2 * opNum;
+            Console.WriteLine("Multiplication math result = " + res2);
+        }
+        //method within class Math
+        public static void sub(int opNum)
+        {
+            int num3 = 5;
+            int res3 = opNum - num3;
+            Console.WriteLine("Subtraction math result = " + res3);
+        }
+        
+            
+
+        
+    }
+}
---
+++ CallMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CallMethod
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+            try
+            {
+                Console.WriteLine("Please enter a number.");
+                //stores user response as integer variable
+                int opNum = Convert.ToInt32(Console.ReadLine());
+                //calls methods from class Math, passing in integer variable opNum
+                Math.add(opNum);
+                Math.mult(opNum);
+                Math.sub(opNum);
+                
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occurred. Please only enter whole numbers.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
