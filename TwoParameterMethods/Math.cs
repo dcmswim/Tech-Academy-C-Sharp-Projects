0994b5a9790beb807838b21d4db10c4a286ebf2e
David <82915335+dcmswim@users.noreply.github.com>
2021-08-07 21:02:33 -0700

    Optional Parameters
    Practice with option parameters in class methods

---
+++ TwoParameterMethods/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoParameterMethods
+{
+    public class Math
+    {   //num2 has a default value. Will only change if user specifies when the method is called by main()
+        public static void math1(int num1, int num2 = 20)
+        {
+            int result = num1 + num2;
+            Console.WriteLine("The result is: " + result);
+        }
+    }
+}
---
+++ TwoParameterMethods/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TwoParameterMethods
+{
+   public class Program
+    {
+       public static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Please enter a number.");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Would you like to enter another number? (Y/N)");
+                string YesOrNo = Console.ReadLine();
+                if (YesOrNo == "Y")
+                {
+                    Console.WriteLine("Please enter the second number.");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    Math.math1(num1, num2);
+                    Console.ReadLine();
+                }
+                //if user does not opt to input second integer only 1 parameter will be passed into math1() function
+                //second parameter for math1() has a default value in the class Math
+                Console.WriteLine("Okay, the computer will choose the second digit. \nCalculating answer...");
+                Math.math1(num1);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Oops! Error occurred. Please only input whole integers and do not divide by 0!");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+
+
+
+        }
+    }
+}
