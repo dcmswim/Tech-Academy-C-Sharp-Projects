09d6ccd00dfe019f86acf09fbf9c10d37d744c56
David <82915335+dcmswim@users.noreply.github.com>
2021-08-11 15:13:44 -0700

    Void Method
    practice passing values into a class method in different ways

---
+++ VoidMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VoidMethod
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        { 
+            int num1 = 5;
+            int num2 = 10;
+            //passing in predefined integer variables
+            Math.add1(num1, num2);
+            //passing in integers as plain integers
+            Math.add1(15, 25);
+            //passing in integer variables by name
+            Math.add1(num1 = 20, num2 = 50);
+        }
+    }
+}
