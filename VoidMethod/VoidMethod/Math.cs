81893ee34f0f32f85ac278fc88ba8c9b52c2e326
David <82915335+dcmswim@users.noreply.github.com>
2021-08-11 17:14:10 -0700

    Add files via upload

---
+++ VoidMethod/VoidMethod/Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    public class Math
+    {
+        public static void add1(int num1, int num2)
+        {
+
+            Console.WriteLine("Please enter a number.");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You entered " + num1 + ". Please enter a second number.");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second number is " + num2 + ". Adding numbers together...");
+            int result = num1 + num2;
+            Console.WriteLine("The result is " + result);
+            
+
+        }
+     }
+
+}
