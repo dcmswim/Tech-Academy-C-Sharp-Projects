2419b7e8d4396b9f6926d03fc4bdcb7d660a384a
David <82915335+dcmswim@users.noreply.github.com>
2021-08-12 16:17:41 -0700

    Add files via upload
    Basic example of class inheritance and class instantiation

---
+++ MethodsAndObjects/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate class 
+            Employee employee= new Employee();
+            //assigning values to class properties
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            //calling superclass method from parent class 'Person' inherited by 'Employee'
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
