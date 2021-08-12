1f6ee83171f37f4fb2c27f76d00f66a21ca28230
David <82915335+dcmswim@users.noreply.github.com>
2021-08-11 19:37:06 -0700

    Add files via upload
    Instantiate class, pass parameters into method and call method

---
+++ VoidMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VoidMethod
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //instantiate class
+            Math add1 = new Math();
+            Math.add1(0, 0); 
+            
+
+
+
+
+
+
+
+
+            //Console.WriteLine("Doing more math...");
+            ////passing in integers as plain integers
+            //Math.add1(15, 25);
+            ////passing in integer variables by name
+            //Math.add1(num1 = 20, num2 = 50);
+        }
+    }
+}
