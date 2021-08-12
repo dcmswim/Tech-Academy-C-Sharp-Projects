d8c72fd73f0a342b9480a056556fdb0aea5b03cf
David <82915335+dcmswim@users.noreply.github.com>
2021-08-11 19:11:26 -0700

    Class Assignment1
    example of output parameters, overloaded function, static classes

---
+++ ClassAssignment1/Math.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment1
+{
+    public class Math
+    {
+        public static void div1(int num1)
+        {
+            int result1 = num1 / 2;
+            Console.WriteLine(num1 + " divided by 2 equals " + result1);
+        }
+
+        //example of overloaded method (same name as above but with more parameters)
+        public static void div1(int num1, int num2)
+        {
+            int result1 = num1 / num2;
+            Console.WriteLine(num1 + " divided by " + num2 + " equals " + result1);
+        }
+
+        //example method that returns outputs 
+        public void getValues(out int x, out int y)
+        {
+            Console.WriteLine("Please enter a value for \"a\": ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter a value for \"b\": ");
+            y = Convert.ToInt32(Console.ReadLine());
+        }
+
+        
+
+    }
+    //example of static class
+    public static class msg
+    {
+        public static void goodbyeMessage()
+        {
+            Console.WriteLine("End of program, goodbye!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassAssignment1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassAssignment1
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            Math div1 = new Math();
+            Console.WriteLine("Please enter a number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Math.div1(num1);
+
+            //example of calling overloaded function
+            Console.WriteLine("Please enter a new number.");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Math.div1(num1, num2);
+
+            // Output parameters example
+            Math n = new Math();
+            //local variable definition
+            int a, b;
+            //calling a function to get the values 
+            n.getValues(out a, out b);
+            Console.WriteLine("The stored value of a is : {0}", a);
+            Console.WriteLine("The store value of b is : {0}", b);
+
+            //example of calling static class method
+            msg.goodbyeMessage();
+        }
+    }
+}
