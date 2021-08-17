f66b61e3cccadce0a014b301bb70195ae2b8433d
David <82915335+dcmswim@users.noreply.github.com>
2021-08-16 20:47:29 -0700

    Add files via upload
    basic example of chaining together constructors (WIP)

---
+++ Constructor/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Person
+    {
+        public string fname {  get; set; }
+        public string lname {  get; set; }
+        public int credits { get; set; }
+
+        public Person(string fname, string lname) : this(50)
+        {
+            Console.WriteLine(fname + lname);
+        }
+
+        public Person(int credits)
+        {
+            
+        }
+
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring a constant variable
+            const string welcome = "Hello! Welcome to this console application.";
+            Console.WriteLine(welcome);
+
+            Console.WriteLine("What is your first name?");
+            string fname = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string lname = Console.ReadLine();
+            //using var keyword to create variable
+            var person = new Person(fname, lname);
+            Console.WriteLine(person.credits);
+            Console.ReadLine();
+        }
+    }
+}
