5e5022cce0719fa8b61e13420f620825df5ca3ac
David <82915335+dcmswim@users.noreply.github.com>
2021-08-18 16:57:09 -0700

    Add files via upload
    Playable TwentyOne(BlackJack) Console Application

---
+++ TwentyOne/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Casino;
+using Casino.TwentyOne;
+using System.Data.SqlClient;
+
+
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////this uses constructor from Player class
+            ////uses constructor to set default balance without user input
+            //Player newPlayer = new Player("Steve");
+
+            //example of a constant variable (used in next Console.WriteLine())
+            const string casinoName = "Grand Hotel and Casino";
+
+            Console.WriteLine("Welcome to the {0}! What is your name?", casinoName);
+            string playerName = Console.ReadLine();
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("How much money did you bring today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("Please enter money as whole digits only.");
+            }
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" ||  answer == "yeah" || answer == "y")
+
+            {   
+                Player player = new Player(playerName, bank);
+                //assigns played id as a Guid
+                player.Id = Guid.NewGuid();
+                //logs player id Guid to external file to keep track of user
+                using (StreamWriter file = new StreamWriter(@"C:\Visual_Studio_Projects\Logs\log.txt", true))
+                {
+                    file.WriteLine(player.Id);
+                }
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException)
+                    {
+                        Console.WriteLine("Fraud alert! Woop woop woop!");
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Oops! An error occurred. Please contact System Administration.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    
+                }
+                game -= player;
+                Console.WriteLine("Thanks for playing!");
+            }
+            Console.WriteLine("Okay, bye!");
+            Console.ReadLine();
+        }
+        private static void UpdateDbWithException(Exception ex)
+        {
+            string connectionString = @"Data Source = (localdb)\ProjectsV13; Initial Catalog = TwentyOneGame; 
+                                        Integrated Security = True; Connect Timeout = 30; Encrypt = False; 
+                                        TrustServerCertificate = False; ApplicationIntent = ReadWrite; 
+                                        MultiSubnetFailover = False";
+
+            string queryString = "INSERT INTO Exceptions (ExceptionType, ExceptionMessage, TimeStamp) " +
+                                  "VALUES (@ExceptionType, @ExceptionMessage, @TimeStamp)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                
+            }
+        }
+    }
+}
